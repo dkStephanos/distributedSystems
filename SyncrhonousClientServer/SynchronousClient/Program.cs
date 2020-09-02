24e41b3706531a0b10b89dab4de1717bbc1d5e8f
dkStephanos <dkstephanos@gmail.com>
2020-09-02 14:09:44 -0400

    Adding syncrhonousClientServer

---
+++ SyncrhonousClientServer/SynchronousClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SynchronousClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Allocate a buffer to store incoming data
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                // 2. Establish a remote endpointfor the socket
+                IPHostEntry IPHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = IPHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                // 3. Create the socket
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                // 4. Connect the socket to the remote endpoint
+                try
+                {
+                    sender.Connect(remoteEP);
+                    Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                    // 5. Encode the data to be sent
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                    // 6. Send the data through the socket
+                    int bytesSent = sender.Send(msg);
+
+                    // 7. Listen for the response (blocking call)
+                    int bytesRec = sender.Receive(bytes);
+
+                    // 8. Process the response
+                    Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // 9. Close the socket
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }  
+        }
+    }
+}
---
+++ SyncrhonousClientServer/SynchronousServer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SynchronousServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Allocate a buffer to store incoming data
+            byte[] bytes = new byte[1024];
+            string data;
+
+            // 2. Establish a loal endpoint for the socket (ip and port)
+            IPHostEntry IPHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = IPHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // 3. Create the socket
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 4. Bind the socket to the local endpoint
+                listener.Bind(localEndPoint);
+
+                // 5. Listen for incoming connections
+                listener.Listen(10);
+
+                // 6. Enter a loop
+                while(true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    Console.WriteLine(ipAddress.ToString());
+                    Console.WriteLine(localEndPoint.ToString());
+
+                    // 6.1. Listen for a connection (blocking call)
+                    Socket handler = listener.Accept();
+
+                    data = "";
+
+                    // 6.2. Process the connection to the read the incoming data
+                    while(true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if(data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // 6.3. Process the incoming data
+                    Console.WriteLine("Text received: {0}", data);
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    handler.Send(msg);
+
+                    // 6.4. Close connection
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("\nPress ENTER to exit...");
+            Console.Read();
+        }
+    }
+}
